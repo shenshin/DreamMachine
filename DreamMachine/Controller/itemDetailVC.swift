a0c5f545eddfdcbdeeea69ffa4341f83fa3d3941
Aleksandr Shenshin <shenshin@me.com>
2017-07-30 04:10:01 +0300

    creating the next (2-nd) ViewController

---
+++ DreamMachine/Controller/itemDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  itemDetailVC.swift
+//  DreamMachine
+//
+//  Created by Aleksandr Shenshin on 29.07.17.
+//  Copyright © 2017 Aleksandr Shenshin. All rights reserved.
+//
+
+import UIKit
+
+class itemDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
--- DreamMachine/View/MaterialView.swift
+++ DreamMachine/View/MaterialView.swift
@@ -6,11 +6,37 @@
 //  Copyright © 2017 Aleksandr Shenshin. All rights reserved.
 //
 
+/// Добавляет стиль красивой рамочки с тенями, а также элемент в меню
+/// Attributes Inspector
+
 import UIKit
 
 private var materialKey = false
 
 extension UIView {
+    
+    @IBInspectable var fuckingDesign: Bool {
+        get {
+            return materialKey
+        }
+        set {
+            materialKey = newValue
+            
+            if materialKey {
+                self.layer.masksToBounds = true
+                self.layer.cornerRadius = 5.0
+                self.layer.shadowOpacity = 0.8
+                self.layer.shadowRadius = 5.0
+                self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+                self.layer.shadowColor = UIColor(red: 157/255, green: 157/255, blue: 157/255, alpha: 1.0).cgColor
+            } else {
+                self.layer.cornerRadius = 0
+                self.layer.shadowOpacity = 0
+                self.layer.shadowRadius = 0
+                self.layer.shadowColor = nil
+            }
+        }
+    }
     
     @IBInspectable var materialDesign: Bool {
         get {
@@ -36,3 +62,4 @@ extension UIView {
     }
     
 }
+
