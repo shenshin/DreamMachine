93ceed122a40ff3298b5882ed6a21da7681dec2a
Aleksandr Shenshin <shenshin@me.com>
2017-07-31 17:14:19 +0300

    заполнение pickerView названиями магазинов из базы данных
    а перед этим заполнение этой базы названиями тех магазинов

--- DreamMachine/Controller/itemDetailVC.swift
+++ DreamMachine/Controller/itemDetailVC.swift
@@ -7,17 +7,76 @@
 //
 
 import UIKit
+import CoreData
 
-class itemDetailVC: UIViewController {
-
+class itemDetailVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: CustomTextField!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
+    //var stores:[Store] = Array()
+    var stores = [Store]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        storePicker.delegate = self
+        storePicker.dataSource = self
+        
         //убирает надпись back рядом с кнопочкой. Возможно,
         //что криво, но лектор предложил еще более жуткий путь
         if let topItem = self.navigationController?.navigationBar.topItem {
             topItem.backBarButtonItem = UIBarButtonItem()
         }
+       
+        getStores()
+        
     }
-
-
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return stores[row].name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        print("pickerView: \(pickerView), row: \(row), component: \(component).")
+    }
+    
+    
+    //совершает запрос к базе данных обо все магазинах и заполняет pickerView этими магазинами
+    func getStores() {
+        let fetchRequest: NSFetchRequest<Store> = Store.fetchRequest()
+        
+        do {
+            self.stores = try context.fetch(fetchRequest)
+            self.storePicker.reloadAllComponents()
+        } catch {
+            let error = error as NSError
+            error.value(forKey: "невозможно получить список магазинов")
+            print(error.description)
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
